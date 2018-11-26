b8d6f056168ab091db0bc77ad37e8538de7a1269
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-11-26 21:06:03 +1100

    Update module.cxx
    - Removed headers from other projects

--- interfaces/python/src/extension/module.cxx
+++ interfaces/python/src/extension/module.cxx
@@ -1,12 +1,6 @@
 #include <boost/python.hpp>
 
 #include "context.hxx"
-#include "lti_system.hxx"
-#include "qr_cost_function.hxx"
-#include "ddp_lqr.hxx"
-#include "generic_system.hxx"
-#include "generic_cost_function.hxx"
-#include "ddp_generic.hxx"
 
 // IMPORTANT: The name in BOOST_PYTHON_MODULE(.) must match that of the .so library file.
 BOOST_PYTHON_MODULE( _acado )
