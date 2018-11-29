34701e95aeec55dfba2c7d767f10139d3000ec22
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-11-29 14:22:57 +1100

    Python Interface Testing
     - Tweaked mockups
     - Adjusted test outputs

--- interfaces/python/test/common.py
+++ interfaces/python/test/common.py
@@ -2,8 +2,8 @@ import acado as ac
 
 def initialize_rocket_context():
     ctx = ac.Context("Rocket")
-    v = ctx.new_differential_state("v")
     s = ctx.new_differential_state("s")
+    v = ctx.new_differential_state("v")
     m = ctx.new_differential_state("m")
     u = ctx.new_control_input("u")
     T = ctx.new_parameter("T")
@@ -12,7 +12,7 @@ def initialize_rocket_context():
     dot_v = ctx.div(ctx.sub(u, ctx.mul(ctx.constant(0.2), ctx.mul(v, v))), m)
     dot_m = ctx.mul( ctx.constant(-0.01), ctx.mul(u, u))
 
-    X = [v, s, m]
+    X = [s, v, m]
     U = [u]
     odes = [ (v, dot_v), (s, dot_s), (m, dot_m)]
     return ctx, X, U, odes, T
